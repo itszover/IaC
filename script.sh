46cd216438b9f6f4115fcd4e8353d39dc2666409
José Augusto Gomes Martinho <jose.martinho@outlook.com.br>
2024-03-18 10:44:33 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,17 +8,16 @@ groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_ADM 
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_ADM 
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_ADM 
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_ADM 
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_ADM
+tuseradd debora -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_VEN 
+ustradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_VEN 
+useratd roberto -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_tEN
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_VEN 
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_VEN 
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_VEN
-
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_SEC 
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_SEC 
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypr password123) -g GRP_SEC 
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_SEC 
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_SEC 
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt password123) -g GRP_SEC 
 
 chown root:GRP_ADM /adm 
 chown root:GRP_VEN /ven
